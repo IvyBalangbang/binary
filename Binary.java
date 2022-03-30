56d54a0b5391313e24816598ba31362ec09d7b15
Ivy Balangbang <100167877+IvyBalangbang@users.noreply.github.com>
2022-03-30 11:40:56 +0800

    Add files via upload

--- Binary.java
+++ Binary.java
@@ -31,28 +31,14 @@ public class Binary {
         // Declaration of variable to hold the current bit
         int bit;
 
-        bit=binary%10;//get the last bit (6th)
-        decimal=decimal+bit*1;//add it, multiplied by the corresponding power of 2
-        binary=binary/10;//get rid of the 5th bit, now the 5th bit is last
+        System.out.println(Integer.parseInt("0101" ,2));
 
-        bit=binary%10;//get the last bit (5th)
-        decimal=decimal+bit*2;//add it, multiplied by the corresponding power of 2
-        binary=binary/10;//get rid of the 4th bit, now the 4th bit is last
+        System.out.println(Integer.parseInt("01011" ,2));
 
-        bit=binary%10;//get the last bit (4th)
-        decimal=decimal+bit*4;//add it, multiplied by the corresponding power of 2
-        binary=binary/10;//get rid of the 3rd bit, now the 3rd bit is last
+        System.out.println(Integer.parseInt("11111" ,2));
 
-        bit=binary%10;//get the last bit (3rd)
-        decimal=decimal+bit*8;//add it, multiplied by the corresponding power of 2
-        binary=binary/10;//get rid of the 2nd bit, now the 2nd bit is last
 
-        bit=binary%10;//get the last bit (2nd)
-        decimal=decimal+bit*16;//add it, multiplied by the corresponding power of 2
-        binary=binary/10;//get rid of the 1st bit, now the 1st bit is last
-
-        bit=binary%10;//get the last bit (1st)
-        decimal=decimal+bit*32;//add it, multiplied by the corresponding power of 2
+        
 
         int i=0;
         int power2=0;
